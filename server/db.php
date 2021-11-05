7fa531c22048b912542152ea858914f14e49fd79
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2021-11-05 19:11:00 -0400

    UI for SignUp and Login page

---
+++ server/db.php
@@ -0,0 +1,29 @@
+<?php
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_password = 'root';
+  $db_db = 'food_lovers';
+ 
+  $mysqli = @new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
+	
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+
+  echo 'Success: A proper connection to MySQL was made.';
+  echo 'Database Name: '.$db_db;
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
+
+  $mysqli->close();
+?>
